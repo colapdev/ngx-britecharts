e3a887373735a94bf28f807eedf39a7a65dd5781
Martin Manzo <manzo.martin@hotmail.com>
2017-09-04 10:57:49 -0300

    Emit event when rendering ready

--- dist/components/grouped-bar-chart/GroupedBarChart.component.d.ts
+++ dist/components/grouped-bar-chart/GroupedBarChart.component.d.ts
@@ -1,7 +1,8 @@
-import { OnInit } from '@angular/core';
+import { OnInit, EventEmitter } from '@angular/core';
 export declare class GroupedBarChartComponent implements OnInit {
     data: any;
     chartConfig: any;
+    ready: EventEmitter<boolean>;
     private groupedBarChart;
     private d3Selection;
     private colors;
