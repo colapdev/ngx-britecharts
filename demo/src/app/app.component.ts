ec9d0cfd524d2915c9555220253b4bf8132970e2
Martin Manzo <manzo.martin@hotmail.com>
2017-09-08 00:42:26 -0300

    Multiline chart with brush demo

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { LegendChartComponent, BarChartComponent, BrushChartComponent } from '@colap-dev/ngx-britecharts/dist';
+import { LegendChartComponent, BarChartComponent, BrushChartComponent, LineChartComponent } from '@colap-dev/ngx-britecharts/dist';
 
 @Component({
   selector: 'app-root',
@@ -27,6 +27,9 @@ export class AppComponent {
   @ViewChild('barChart') barChart: BarChartComponent;
   @ViewChild('legendChart') legendChart: LegendChartComponent;
   @ViewChild('brushChart') brushChart: BrushChartComponent;
+  @ViewChild('multilineChart') multilineChart: LineChartComponent;
+  @ViewChild('multilineBrushChart') multilineBrushChart: BrushChartComponent;
+
 
   private onBarChartClick($ev) {
     console.log($ev);
@@ -281,10 +284,48 @@ export class AppComponent {
       title: 'Quantity Sold'
     }
   };
+  public multilineBrushChartConfig = {
+    properties: {
+      height: 125,
+      margin: { top: 0, bottom: 0, left: 70, right: 30 }
+    }
+  };
+
+  public configCustomEventsMultilineBrushChartConfig(ready) {
+    if (ready) {
+      let that = this;
+      this.multilineBrushChart.brush.on('customBrushEnd', function(brushExtent) {
+        that.filterMultilineChartData(brushExtent[0], brushExtent[1]);
+      });
+    }
+  }
+
+  private filterMultilineChartData(startDate, endDate) {
+    console.log(startDate, endDate)
+    if(startDate == null && endDate == null) {
+      return this.multilineChartData;
+    }
+
+    let data = {};
+    let that = this;
+    data["dataByDate"] = this.multilineChartData["dataByDate"].filter(this.isInRange.bind(null, startDate, endDate));
+    data["dataByTopic"] = this.multilineChartData["dataByTopic"].map((topic) => {
+        topic.dates = topic.dates.filter(that.isInRange.bind(null, startDate, endDate));
+        return topic;
+    });
+    this.multilineChart.data = data;
+    this.multilineChart.redrawChart();
+  }
+
+  private isInRange(d0, d1, d) {
+      return new Date(d.date) >= d0 && new Date(d.date) <= d1;
+  }
 
   public brushChartData = [{ "date": "2015-06-27T07:00:00.000Z", "value": 4 }, { "date": "2015-06-28T07:00:00.000Z", "value": 12 }, { "date": "2015-06-29T07:00:00.000Z", "value": 33 }, { "date": "2015-06-30T07:00:00.000Z", "value": 17 }, { "date": "2015-07-01T07:00:00.000Z", "value": 17 }, { "date": "2015-07-02T07:00:00.000Z", "value": 16 }, { "date": "2015-07-03T07:00:00.000Z", "value": 8 }, { "date": "2015-07-04T07:00:00.000Z", "value": 14 }, { "date": "2015-07-05T07:00:00.000Z", "value": 11 }, { "date": "2015-07-06T07:00:00.000Z", "value": 14 }, { "date": "2015-07-07T07:00:00.000Z", "value": 25 }, { "date": "2015-07-08T07:00:00.000Z", "value": 55 }, { "date": "2015-07-09T07:00:00.000Z", "value": 15 }, { "date": "2015-07-10T07:00:00.000Z", "value": 26 }, { "date": "2015-07-11T07:00:00.000Z", "value": 21 }, { "date": "2015-07-12T07:00:00.000Z", "value": 16 }, { "date": "2015-07-13T07:00:00.000Z", "value": 20 }, { "date": "2015-07-14T07:00:00.000Z", "value": 26 }, { "date": "2015-07-15T07:00:00.000Z", "value": 24 }, { "date": "2015-07-16T07:00:00.000Z", "value": 29 }, { "date": "2015-07-17T07:00:00.000Z", "value": 12 }, { "date": "2015-07-18T07:00:00.000Z", "value": 16 }, { "date": "2015-07-19T07:00:00.000Z", "value": 11 }, { "date": "2015-07-20T07:00:00.000Z", "value": 29 }, { "date": "2015-07-21T07:00:00.000Z", "value": 9 }, { "date": "2015-07-22T07:00:00.000Z", "value": 26 }, { "date": "2015-07-23T07:00:00.000Z", "value": 21 }, { "date": "2015-07-24T07:00:00.000Z", "value": 18 }, { "date": "2015-07-25T07:00:00.000Z", "value": 15 }, { "date": "2015-07-26T07:00:00.000Z", "value": 23 }, { "date": "2015-07-27T07:00:00.000Z", "value": 43 }, { "date": "2015-07-28T07:00:00.000Z", "value": 44 }, { "date": "2015-07-29T07:00:00.000Z", "value": 67 }, { "date": "2015-07-30T07:00:00.000Z", "value": 67 }, { "date": "2015-07-31T07:00:00.000Z", "value": 0 }, { "date": "2015-08-01T07:00:00.000Z", "value": 0 }, { "date": "2015-08-02T07:00:00.000Z", "value": 0 }];
   public brushChartConfig = {
-    height: 125
+    properties: {
+      height: 125
+    }
   };
 
   public configCustomEventsBrushChart(ready) {
@@ -298,4 +339,5 @@ export class AppComponent {
       });
     }
   }
+
 }
