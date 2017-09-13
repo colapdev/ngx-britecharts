729384690f84338de651808e9c58f1ba79894e07
Martin Manzo <manzo.martin@hotmail.com>
2017-09-12 21:03:11 -0300

    Fixed brush chart demo

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -301,24 +301,44 @@ export class AppComponent {
   }
 
   private filterMultilineChartData(startDate, endDate) {
-    console.log(startDate, endDate)
-    if(startDate == null && endDate == null) {
+    if (startDate == null && endDate == null) {
       return this.multilineChartData;
     }
+    let iDate = new Date(startDate);
+    let eDate = new Date(endDate);
 
     let data = {};
     let that = this;
-    data["dataByDate"] = this.multilineChartData["dataByDate"].filter(this.isInRange.bind(null, startDate, endDate));
-    data["dataByTopic"] = this.multilineChartData["dataByTopic"].map((topic) => {
-        topic.dates = topic.dates.filter(that.isInRange.bind(null, startDate, endDate));
-        return topic;
-    });
+    data["dataByDate"] = [];
+    data["dataByTopic"] = [];
+    
+    for (let d of this.multilineChartData["dataByDate"]) {
+      let aDate = new Date(d["date"]);
+      if (iDate <= aDate && aDate <= eDate) {
+        data["dataByDate"].push(d);
+      }
+    }
+
+    for (let t of this.multilineChartData["dataByTopic"]) {
+      let newTopic = {};
+      newTopic["topic"] = t["topic"];
+      newTopic["topicName"] = t["topicName"];
+      newTopic["dates"] = [];
+      for (let d of t["dates"]) {
+        let aDate = new Date(d["date"]);
+        if (iDate <= aDate && aDate <= eDate) {
+          newTopic["dates"].push({
+            'date': aDate,
+            'value': d["value"],
+            'fullDate': d["date"]
+          });
+        }
+      }
+      data["dataByTopic"].push(newTopic);
+    }
+
     this.multilineChart.data = data;
     this.multilineChart.redrawChart();
-  }
-
-  private isInRange(d0, d1, d) {
-      return new Date(d.date) >= d0 && new Date(d.date) <= d1;
   }
 
   public brushChartData = [{ "date": "2015-06-27T07:00:00.000Z", "value": 4 }, { "date": "2015-06-28T07:00:00.000Z", "value": 12 }, { "date": "2015-06-29T07:00:00.000Z", "value": 33 }, { "date": "2015-06-30T07:00:00.000Z", "value": 17 }, { "date": "2015-07-01T07:00:00.000Z", "value": 17 }, { "date": "2015-07-02T07:00:00.000Z", "value": 16 }, { "date": "2015-07-03T07:00:00.000Z", "value": 8 }, { "date": "2015-07-04T07:00:00.000Z", "value": 14 }, { "date": "2015-07-05T07:00:00.000Z", "value": 11 }, { "date": "2015-07-06T07:00:00.000Z", "value": 14 }, { "date": "2015-07-07T07:00:00.000Z", "value": 25 }, { "date": "2015-07-08T07:00:00.000Z", "value": 55 }, { "date": "2015-07-09T07:00:00.000Z", "value": 15 }, { "date": "2015-07-10T07:00:00.000Z", "value": 26 }, { "date": "2015-07-11T07:00:00.000Z", "value": 21 }, { "date": "2015-07-12T07:00:00.000Z", "value": 16 }, { "date": "2015-07-13T07:00:00.000Z", "value": 20 }, { "date": "2015-07-14T07:00:00.000Z", "value": 26 }, { "date": "2015-07-15T07:00:00.000Z", "value": 24 }, { "date": "2015-07-16T07:00:00.000Z", "value": 29 }, { "date": "2015-07-17T07:00:00.000Z", "value": 12 }, { "date": "2015-07-18T07:00:00.000Z", "value": 16 }, { "date": "2015-07-19T07:00:00.000Z", "value": 11 }, { "date": "2015-07-20T07:00:00.000Z", "value": 29 }, { "date": "2015-07-21T07:00:00.000Z", "value": 9 }, { "date": "2015-07-22T07:00:00.000Z", "value": 26 }, { "date": "2015-07-23T07:00:00.000Z", "value": 21 }, { "date": "2015-07-24T07:00:00.000Z", "value": 18 }, { "date": "2015-07-25T07:00:00.000Z", "value": 15 }, { "date": "2015-07-26T07:00:00.000Z", "value": 23 }, { "date": "2015-07-27T07:00:00.000Z", "value": 43 }, { "date": "2015-07-28T07:00:00.000Z", "value": 44 }, { "date": "2015-07-29T07:00:00.000Z", "value": 67 }, { "date": "2015-07-30T07:00:00.000Z", "value": 67 }, { "date": "2015-07-31T07:00:00.000Z", "value": 0 }, { "date": "2015-08-01T07:00:00.000Z", "value": 0 }, { "date": "2015-08-02T07:00:00.000Z", "value": 0 }];
